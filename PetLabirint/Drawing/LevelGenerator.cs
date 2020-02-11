bec5f074acbf960e0f2e7d3885ee0f1a4ce14f5d
barbocc <barbocc@gmail.com>
2020-02-11 15:44:54 +1000

    first commit

---
+++ PetLabirint/Drawing/LevelGenerator.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+
+namespace PetLabirint
+{
+    class LevelGenerator
+    {
+        public Texture2D BackgroundTexture { get; set; }
+
+        public LevelGenerator(Texture2D levelTexture)
+        {
+            BackgroundTexture = levelTexture;
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+
+            int width = BackgroundTexture.Width;
+            int height = BackgroundTexture.Height;
+
+            int cols = (int)((float)GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Width / (float)width);
+            int rows = (int)((float)GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Height / (float)height); 
+
+            for (int i=0; i<cols; i++)
+            {
+               
+                for (int j = 0; j < rows; j++)
+                {
+                    Rectangle testRectangle = new Rectangle(i*width, j*height, width, height);
+                    spriteBatch.Begin();
+                    spriteBatch.Draw(BackgroundTexture, testRectangle, Color.White);
+                    spriteBatch.End();
+                }
+
+            }
+
+        }
+    }
+}
---
+++ PetLabirint/Players/Player.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetLabirint
+{
+    class Player
+    {
+        private string name;
+        private int score;
+        private int livesLeft;
+
+        public Player(string name)
+        {
+            this.name = name;
+            this.score = 0;
+            this.livesLeft = 3;
+            
+        }
+
+        public Player(string name, int startingLives)
+        {
+            this.name = name;
+            this.score = 0;
+            this.livesLeft = startingLives;
+        }
+
+        public string GetName()
+        {
+            return this.name;
+        }
+
+        public int GetScore()
+        {
+            return this.score;
+        }
+
+        public int GetLivesLeft()
+        {
+            return this.livesLeft;
+        }
+
+        public void Kill()
+        {
+            if (this.livesLeft > 0)
+            {
+                this.livesLeft--;
+            }
+        }
+
+    }
+}
